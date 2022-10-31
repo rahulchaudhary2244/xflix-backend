8bd0bfcfa61ea1bff595766a25223352d021fb16
rahulchaudhary2244 <rahulchaudhary2244@gmail.com>
2022-10-31 22:29:16 +0530

    added insert many api

--- src/routes/v1/video.route.js
+++ src/routes/v1/video.route.js
@@ -1,46 +1,49 @@
-const router = require("express").Router();
-const { videoController } = require("../../controllers/index");
+const router = require('express').Router();
+const { videoController } = require('../../controllers/index');
 const {
-  saveVideo,
-  getAllVideos,
-  getVideoById,
-  incrementVideoViewCountById,
-  changeVideoVoteCountById,
+    saveVideo,
+    getAllVideos,
+    getVideoById,
+    incrementVideoViewCountById,
+    changeVideoVoteCountById,
+    dropAndInsertMany,
 } = videoController;
 const {
-  validateJoiSchema_body,
-  validateJoiSchema_params,
-  validateJoiSchema_query,
-} = require("../../middlewares/validateJoiSchema");
-const videoValidation = require("../../validations/index");
+    validateJoiSchema_body,
+    validateJoiSchema_params,
+    validateJoiSchema_query,
+} = require('../../middlewares/validateJoiSchema');
+const videoValidation = require('../../validations/index');
 
 const validateSaveVideo = validateJoiSchema_body(
-  videoValidation.saveVideoValidation
+    videoValidation.saveVideoValidation
 );
 
 const validateMongoId = validateJoiSchema_params(videoValidation.mongoId);
 
 const validateUpdateVotes = validateJoiSchema_body(
-  videoValidation.updateVideoVotes
+    videoValidation.updateVideoVotes
 );
 
 const validateGetBySearchQuery = validateJoiSchema_query(
-  videoValidation.getBySearchQuery
+    videoValidation.getBySearchQuery
 );
 
-router.post("/", validateSaveVideo, saveVideo);
+router.post('/', validateSaveVideo, saveVideo);
 
-router.get("/", validateGetBySearchQuery,getAllVideos);
+router.get('/', validateGetBySearchQuery, getAllVideos);
 
-router.get("/:videoId", validateMongoId, getVideoById);
+router.get('/:videoId', validateMongoId, getVideoById);
 
-router.patch("/:videoId/views", validateMongoId, incrementVideoViewCountById);
+router.patch('/:videoId/views', validateMongoId, incrementVideoViewCountById);
 
 router.patch(
-  "/:videoId/votes",
-  validateMongoId,
-  validateUpdateVotes,
-  changeVideoVoteCountById
+    '/:videoId/votes',
+    validateMongoId,
+    validateUpdateVotes,
+    changeVideoVoteCountById
 );
+
+router.post('/delete-and-insert-many', dropAndInsertMany);
 
 module.exports = router;
--- src/services/video.services.js
+++ src/services/video.services.js
@@ -1,9 +1,10 @@
-const { VideoModel } = require("../models/index");
-const httpStatus = require("http-status");
-const ApiError = require("../utils/ApiError");
+const { VideoModel } = require('../models/index');
+const httpStatus = require('http-status');
+const ApiError = require('../utils/ApiError');
+const videoData = require('../data/videos');
 
 class VideoService {
-  /** saves new video to DB
+    /** saves new video to DB
    * @param {Object} videoBody
    * @returns {Promise<Video>}
    * @throws {ApiError}
@@ -35,78 +36,89 @@ class VideoService {
 }
 
    */
-  save = async (videoBody) => {
-    const newVideo = new VideoModel(videoBody);
-    const result = await newVideo.save();
-    if (result) return result;
+    save = async (videoBody) => {
+        const newVideo = new VideoModel(videoBody);
+        const result = await newVideo.save();
+        if (result) return result;
 
-    throw new ApiError(httpStatus.BAD_REQUEST, "Video not saved");
-  };
+        throw new ApiError(httpStatus.BAD_REQUEST, 'Video not saved');
+    };
 
-  getAll = async () => await VideoModel.find({});
+    getAll = async () => await VideoModel.find({});
 
-  getBySearchQuery = async (searchQuery) => {
-    const { title, genres, contentRating, sortBy } = searchQuery;
+    getBySearchQuery = async (searchQuery) => {
+        const { title, genres, contentRating, sortBy } = searchQuery;
 
-    // sort in descending order by sortBy key provided
-    let result = await VideoModel.find({
-      $and: [
-        {
-          title: { $regex: title, $options: "i" },
-        },
-        {
-          genre: {
-            $in: genres,
-          },
-        },
-        {
-          contentRating: {
-            $in: contentRating,
-          },
-        },
-      ],
-    }).sort({ [sortBy]: -1 });
+        // sort in descending order by sortBy key provided
+        let result = await VideoModel.find({
+            $and: [
+                {
+                    title: { $regex: title, $options: 'i' },
+                },
+                {
+                    genre: {
+                        $in: genres,
+                    },
+                },
+                {
+                    contentRating: {
+                        $in: contentRating,
+                    },
+                },
+            ],
+        }).sort({ [sortBy]: -1 });
 
-    return result;
-  };
+        return result;
+    };
 
-  getById = async (_id) => {
-    const video = await VideoModel.findById({ _id });
-    if (video) return video;
+    getById = async (_id) => {
+        const video = await VideoModel.findById({ _id });
+        if (video) return video;
 
-    throw new ApiError(httpStatus.NOT_FOUND, "No video found with matching id");
-  };
+        throw new ApiError(
+            httpStatus.NOT_FOUND,
+            'No video found with matching id'
+        );
+    };
 
-  incrementViewCountById = async (_id) => {
-    const video = await VideoModel.findById({ _id });
-    if (video) {
-      video.viewCount += 1;
-      await video.save();
-    } else {
-      throw new ApiError(
-        httpStatus.NO_CONTENT,
-        "No video found with matching id"
-      );
-    }
-  };
+    incrementViewCountById = async (_id) => {
+        const video = await VideoModel.findById({ _id });
+        if (video) {
+            video.viewCount += 1;
+            await video.save();
+        } else {
+            throw new ApiError(
+                httpStatus.NO_CONTENT,
+                'No video found with matching id'
+            );
+        }
+    };
 
-  changeVoteCountById = async (_id, vote, change) => {
-    const video = await VideoModel.findById({ _id });
+    changeVoteCountById = async (_id, vote, change) => {
+        const video = await VideoModel.findById({ _id });
 
-    if (video) {
-      //`${vote}s` can only be upVotes or downVotes
-      if (change === "increase") video.votes[`${vote}s`] += 1;
-      else if (change === "decrease") video.votes[`${vote}s`] -= 1;
+        if (video) {
+            //`${vote}s` can only be upVotes or downVotes
+            if (change === 'increase') video.votes[`${vote}s`] += 1;
+            else if (change === 'decrease') video.votes[`${vote}s`] -= 1;
 
-      if (video.votes[`${vote}s`] < 0) video.votes[`${vote}s`] = 0;
-      await video.save();
-    } else {
-      throw new ApiError(
-        httpStatus.NO_CONTENT,
-        "No video found with matching id"
-      );
-    }
-  };
+            if (video.votes[`${vote}s`] < 0) video.votes[`${vote}s`] = 0;
+            await video.save();
+        } else {
+            throw new ApiError(
+                httpStatus.NO_CONTENT,
+                'No video found with matching id'
+            );
+        }
+    };
+
+    dropAndInsertMany = async () => {
+        await VideoModel.deleteMany();
+        const videos = await VideoModel.insertMany(videoData, {
+            ordered: false,
+        });
+        return videos;
+    };
 }
 
 module.exports.VideoService = VideoService;
