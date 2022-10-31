d1e1950fde9cad2e917f22375ffb8a098ba52ccd
rahulchaudhary2244 <rahulchaudhary2244@gmail.com>
2022-10-31 21:27:10 +0530

    port changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { errorHandler } = require('./middlewares/error');
+const httpStatus = require('http-status');
+const v1Routes = require('./routes/v1/index');
 
-const { app } = require('./app');
+const app = express();
 
-const APP_PORT = process.env.APP_PORT;
+app.use(express.json());
+
+app.use(cors());
+app.options('*', cors());
+
+app.use('/v1', v1Routes);
+
+app.use((req, res, next) => {
+    next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+});
+
+app.use(errorHandler);
+
+const APP_PORT = process.env.PORT || 5000;
 const MONGODB_URL = process.env.MONGODB_URL;
 
 app.listen(APP_PORT, () => console.log(`Server listening at port ${APP_PORT}`));
